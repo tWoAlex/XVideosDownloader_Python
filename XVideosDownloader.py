8d15eab405191a2b13cd9089b602de78a14c439c
tWoAlex <94020738+tWoAlex@users.noreply.github.com>
2022-08-15 17:55:36 +0500

    Title and preview
    Title and preview extraction methods

--- XVideosDownloader.py
+++ XVideosDownloader.py
@@ -1,3 +1,13 @@
+from XVideo import XVideo as xv
+
 # link='https://www.xvideos.com/video68984225/_erasmus_'
 # link = 'https://www.xvideos.com/video7388507/_'
 link='https://www.xvideos.com/video49521087/_._'
+
+a=xv(URL=link)
+a.Retrieve()
+print(a.title)
+print(a.preview_name)
+
+with open(a.preview_name,'wb') as result:
+    result.write(a.preview_bytes)
